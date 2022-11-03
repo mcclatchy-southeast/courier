2c9ddf783fd722fa67960a78a2b0ac0d0c6bde57
Tyler Dukes <tyler.dukes@gmail.com>
2022-11-03 15:03:54 -0400

    adding basic postgres fetch

---
+++ R/fetch_db_data.R
@@ -0,0 +1,56 @@
+#' Retrieve a chunk of data
+#'
+#' Use Postgres to access a specified database and retrieve a chunk of data
+#' in a specified size as a `dplyr` tibble.
+#'
+#' @param dbname Name of the database. Default is `NULL`.
+#' @param host Name of the host IP address. Default is `NULL`.
+#' @param user Username. Default is `NULL`.
+#' @param port Port number, typically 5432. Default is `NULL`.
+#' @param table_name Name of the table you want to query data
+#' @param order Default is `1`, or the data's first column.
+#' @param limit Default is `10000`, consistent with Placekey bulk API rate limiting.
+#' @param offset Number of rows to skip in the data. Default is `0`.
+#'
+#' @import DBI
+#' @import RPostgres
+#' @import dplyr
+#'
+#' @return A tibble chunked from the specified database table.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' fetch_db_data(table_name = 'mtcars', offset = 5)
+#' fetch_db_data('exampledb', '123.456.7.89', 'user', 'mtcars')
+#' }
+fetch_db_data <- function(dbname = NULL,
+                          host = NULL,
+                          user = NULL,
+                          port = NULL,
+                          table_name,
+                          order = 1,
+                          limit = 10000,
+                          offset = 0){
+  #establish the connection
+  con <- con <- DBI::dbConnect(
+    drv = RPostgres::Postgres(),
+    dbname = dbname,
+    host = host,
+    port = port,
+    user = user)
+
+  #construct the query
+  query = paste('SELECT *',
+                'FROM', table_name,
+                'ORDER BY', order,
+                'LIMIT', limit, 'OFFSET', offset,
+                ';',
+                sep = ' ')
+
+  #get the data from the specified table
+  df_chunk <- dplyr::tibble(dbGetQuery(con, query))
+
+  return(df_chunk)
+
+}
